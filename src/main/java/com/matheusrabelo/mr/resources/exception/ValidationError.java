e72e26a2fae01e306d41e4e947563cb4d744bff1
matheus rabelo <matheusrabelo520@gmail.com>
2021-06-22 21:13:36 -0300

    Padronizando as exececoes

--- src/main/java/com/matheusrabelo/mr/resources/exception/ValidationError.java
+++ src/main/java/com/matheusrabelo/mr/resources/exception/ValidationError.java
@@ -9,9 +9,8 @@ public class ValidationError extends StandardError {
 
 	private List<FieldMessage> errors = new ArrayList<>();
 
-	public ValidationError(Integer status, String msg, long timeStamp) {
-		super(status, msg, timeStamp);
-
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
 	}
 
 	public List<FieldMessage> getErrors() {
@@ -20,7 +19,5 @@ public class ValidationError extends StandardError {
 
 	public void addError(String fieldName, String messagem) {
 		errors.add(new FieldMessage(fieldName, messagem));
-
 	}
-
 }
